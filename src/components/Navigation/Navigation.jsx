616be9bab5802ce79f6cb547beaebbaccabe5f50
Starboy <samskywalker001@gmail.com>
2024-02-12 19:43:52 +0530

    Deployment

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,17 +5,20 @@ export function Navigation({ onRouteChange, isSignedIn }) {
             <div>
                 <nav style={{ display: "flex", justifyContent: "flex-end" }}>
                     <a className="f5 link dim underline pa3 white-70 pointer mt2 mr2"
-                        onClick={() => onRouteChange('sign-out')}>Sign Out
+                        onClick={() => onRouteChange('sign-out')}
+                        href={() => false}>Sign Out
                     </a>
                 </nav>
             </div>
             : <div>
                 <nav style={{ display: "flex", justifyContent: "flex-end" }}>
                     <a className="f5 link dim underline pa3 white-70 pointer mt2 mr2"
-                        onClick={() => onRouteChange('sign-out')}>Sign In/Sign Up   {/* Sign Out! */}
+                        onClick={() => onRouteChange('sign-out')}
+                        href={() => false}>Sign In/Sign Up   {/* Sign Out! */}
                     </a>
                     <a className="f5 link dim underline pa3 white-70 pointer mt2 mr2"
-                        onClick={() => onRouteChange('face-detect')}>Guest Log In
+                        onClick={() => onRouteChange('face-detect')}
+                        href={() => false}>Guest Log In
                     </a>
                 </nav>
             </div>
